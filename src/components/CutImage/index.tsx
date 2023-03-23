7681f07c7f95c6f7d626d3f3a37e4167d8d4f9df
Hasegawa-Akito <h.akito1999@gmail.com>
2023-03-23 15:46:57 +0900

    カスタムhooks作成

--- src/components/CutImage/index.tsx
+++ src/components/CutImage/index.tsx
@@ -2,11 +2,16 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import InputImage from "./InputImage";
 import * as poseDetection from "@tensorflow-models/pose-detection";
 import "@tensorflow/tfjs-backend-webgl";
+import { useCutImage } from "../hooks/useCutImage";
 
 const CutImage = () => {
   const [detector, setDetector] = useState<poseDetection.PoseDetector>();
   const [imageURL, setImageURL] = useState<string | undefined>();
   const imageRef = useRef<HTMLImageElement>(null);
+  const cutImageTool = useCutImage({
+    image: imageRef.current,
+    detector: detector,
+  });
 
   // あらかじめモデルを生成しておく
   useEffect(() => {
@@ -18,59 +23,12 @@ const CutImage = () => {
     })();
   }, []);
 
-  const cutImage = async () => {
-    const image = imageRef.current;
-    if (!detector || !image) return;
+  useEffect(() => {
+    if (!cutImageTool.cutImageURL) return;
 
-    const estimationConfig = {
-      maxPoses: 5,
-      flipHorizontal: false,
-      scoreThreshold: 0.5,
-      nmsRadius: 20,
-    };
-    const poses = await detector.estimatePoses(image, estimationConfig);
+    setImageURL(cutImageTool.cutImageURL);
+  }, [cutImageTool.cutImageURL]);
 
-    if (!poses.length) return;
-    const left_shoulder = poses[0].keypoints[5];
-    const right_shoulder = poses[0].keypoints[6];
-    const left_hip = poses[0].keypoints[11];
-    const right_hip = poses[0].keypoints[12];
-    const left_elbow = poses[0].keypoints[7];
-    const right_elbow = poses[0].keypoints[8];
-
-    const crop = {
-      x: right_elbow.x,
-      y: right_shoulder.y,
-      width: left_elbow.x - right_elbow.x,
-      height: right_hip.y - right_shoulder.y,
-      unit: "px",
-    };
-
-    const canvas = document.createElement("canvas");
-    const pixelRatio = window.devicePixelRatio;
-    const scaleX = image.naturalWidth / image.width;
-    const scaleY = image.naturalHeight / image.height;
-    const ctx = canvas.getContext("2d");
-
-    canvas.width = crop.width * pixelRatio * scaleX;
-    canvas.height = crop.height * pixelRatio * scaleY;
-
-    ctx!.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-    ctx!.imageSmoothingQuality = "high";
-    ctx!.drawImage(
-      image,
-      crop.x * scaleX,
-      crop.y * scaleY,
-      crop.width * scaleX,
-      crop.height * scaleY,
-      0,
-      0,
-      crop.width * scaleX,
-      crop.height * scaleY
-    );
-    const cutImgURL = canvas.toDataURL("image/jpeg", 1.0);
-    setImageURL(cutImgURL);
-  };
   return (
     <div className="bg-gray-800 min-h-screen w-full">
       <div className="w-full py-10 text-center">
@@ -91,7 +49,7 @@ const CutImage = () => {
         imageURL={imageURL}
         setImageURL={setImageURL}
       />
-      <div onClick={() => cutImage()}>予測</div>
+      <div onClick={() => cutImageTool.cutImage()}>予測</div>
     </div>
   );
 };
--- src/components/hooks/useCutImage.tsx
+++ src/components/hooks/useCutImage.tsx
@@ -1,7 +1,79 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+import * as poseDetection from "@tensorflow-models/pose-detection";
 
-export const useCountTime = () => {
+type Props = {
+  image: HTMLImageElement | null;
+  detector: poseDetection.PoseDetector | undefined;
+};
+export const useCutImage = ({ image, detector }: Props) => {
   const [cutImageURL, setCutImageURL] = useState<string | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  return cutImageURL;
+  const cutImage = useCallback(async () => {
+    try {
+      if (!detector || !image) {
+        throw new Error("モデルの生成失敗失敗または画像の未取得");
+      }
+
+      setIsLoading(true);
+
+      const estimationConfig = {
+        maxPoses: 5,
+        flipHorizontal: false,
+        scoreThreshold: 0.5,
+        nmsRadius: 20,
+      };
+      const poses = await detector.estimatePoses(image, estimationConfig);
+
+      if (!poses.length) return;
+      const left_shoulder = poses[0].keypoints[5];
+      const right_shoulder = poses[0].keypoints[6];
+      const left_hip = poses[0].keypoints[11];
+      const right_hip = poses[0].keypoints[12];
+      const left_elbow = poses[0].keypoints[7];
+      const right_elbow = poses[0].keypoints[8];
+
+      const crop = {
+        x: right_elbow.x,
+        y: right_shoulder.y,
+        width: left_elbow.x - right_elbow.x,
+        height: right_hip.y - right_shoulder.y,
+        unit: "px",
+      };
+
+      const canvas = document.createElement("canvas");
+      const pixelRatio = window.devicePixelRatio;
+      const scaleX = image.naturalWidth / image.width;
+      const scaleY = image.naturalHeight / image.height;
+      const ctx = canvas.getContext("2d");
+
+      canvas.width = crop.width * pixelRatio * scaleX;
+      canvas.height = crop.height * pixelRatio * scaleY;
+
+      ctx!.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+      ctx!.imageSmoothingQuality = "high";
+      ctx!.drawImage(
+        image,
+        crop.x * scaleX,
+        crop.y * scaleY,
+        crop.width * scaleX,
+        crop.height * scaleY,
+        0,
+        0,
+        crop.width * scaleX,
+        crop.height * scaleY
+      );
+
+      const cutImgURL = canvas.toDataURL("image/jpeg", 1.0);
+
+      setCutImageURL(cutImgURL);
+      setIsLoading(false);
+    } catch (e) {
+      setIsLoading(false);
+      setError(true);
+    }
+  }, [image]);
+
+  return { cutImageURL, cutImage, isLoading, error };
 };
